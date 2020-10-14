b6f6ebc3172d5e233f715e47534100efb0cc9ea9
unknown <yousefmaraqa98@gmail.com>
2020-10-14 10:03:47 +0300

    Updates

---
+++ precache-manifest.10b04f7e9efbc4d4d9918765db586e05.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c40d50c266f2bdcaf67de459699d171",
+    "url": "/E-learning-demo/index.html"
+  },
+  {
+    "revision": "c26c4ba08b4654aa4e0a",
+    "url": "/E-learning-demo/static/css/main.a9ef5879.chunk.css"
+  },
+  {
+    "revision": "32cc44679d78953e3919",
+    "url": "/E-learning-demo/static/js/2.ce178ee4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/E-learning-demo/static/js/2.ce178ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26c4ba08b4654aa4e0a",
+    "url": "/E-learning-demo/static/js/main.3f30b4e0.chunk.js"
+  },
+  {
+    "revision": "c8e35190cf8b7daa3025",
+    "url": "/E-learning-demo/static/js/runtime-main.b05450a9.js"
+  },
+  {
+    "revision": "4518b6f067e234d6233006497cd0b36d",
+    "url": "/E-learning-demo/static/media/GothamRounded-Medium.4518b6f0.otf"
+  },
+  {
+    "revision": "a25457125720b406343c119dbc22086f",
+    "url": "/E-learning-demo/static/media/ReadingOurpleTab.a2545712.svg"
+  },
+  {
+    "revision": "5317bd403fb88c3d6b63cda9d98dc32e",
+    "url": "/E-learning-demo/static/media/listenOrangeTab.5317bd40.svg"
+  },
+  {
+    "revision": "3755c8871446aaeb9d7928501298c473",
+    "url": "/E-learning-demo/static/media/main-logo.3755c887.png"
+  },
+  {
+    "revision": "effaac64ff220abe0a01f28e56151ce0",
+    "url": "/E-learning-demo/static/media/tabgray.effaac64.svg"
+  },
+  {
+    "revision": "83f452d61e4b0ee8781a1de253f6dc36",
+    "url": "/E-learning-demo/static/media/vocabluetab.83f452d6.svg"
+  },
+  {
+    "revision": "3d500259f0d62d043e3089c09a7b7025",
+    "url": "/E-learning-demo/static/media/writeblueTab.3d500259.svg"
+  }
+]);
